de66ff40a30d76c2f3dcd7a9ecf081802d5fd413
petergerrit <peter.vandergriend@tutamail.com>
2025-11-14 13:35:53 -0600

    correct main.py to run step 2; epsilon=0.5, 0.6, 0.7 Trotter evolution runs

--- main.py
+++ main.py
@@ -70,13 +70,13 @@ from params import *
 # In[ ]:
 
 
-for i in range(3):
-    num_shots = 1024
-    epsilon = np.round(0.2*(1+i), 1)
-    circuit = trotter_stepper(1, Nqbits, epsilon, mass, mid).decompose().decompose()
-    circuit.measure_all()
-    with open('data/step1_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
-         pickle.dump(circuit_knitter(circuit, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
+# for i in range(3):
+#     num_shots = 1024
+#     epsilon = np.round(0.2*(1+i), 1)
+#     circuit = trotter_stepper(1, Nqbits, epsilon, mass, mid).decompose().decompose()
+#     circuit.measure_all()
+#     with open('data/step1_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
+#          pickle.dump(circuit_knitter(circuit, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 # In[ ]:
@@ -87,7 +87,7 @@ for i in range(3):
     epsilon = np.round((5+i)*0.1, 1)
     circuit = trotter_stepper(2, Nqbits, epsilon, mass, mid).decompose().decompose()
     circuit.measure_all()
-    with open('data/step1_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
+    with open('data/step2_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
          pickle.dump(circuit_knitter(circuit, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
