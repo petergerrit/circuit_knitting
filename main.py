02978c4d78374f035e62cfdc2d470ba3c1f259d7
petergerrit <peter.vandergriend@tutamail.com>
2025-11-14 12:59:08 -0600

    correct error in main.py to run oisy Trotter evolution steps

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ from params import *
 #         pickle.dump(circuit_knitter(trot_step_2, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# In[3]:
+# In[ ]:
 
 
 for i in range(3):
@@ -76,7 +76,19 @@ for i in range(3):
     circuit = trotter_stepper(1, Nqbits, epsilon, mass, mid).decompose().decompose()
     circuit.measure_all()
     with open('data/step1_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
-         pickle.dump(circuit_knitter(trot_step_1, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
+         pickle.dump(circuit_knitter(circuit, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+# In[ ]:
+
+
+for i in range(3):
+    num_shots = 1024
+    epsilon = np.round((5+i)*0.1, 1)
+    circuit = trotter_stepper(2, Nqbits, epsilon, mass, mid).decompose().decompose()
+    circuit.measure_all()
+    with open('data/step1_epsilon' + str(epsilon)[0] + str(epsilon)[2] + '_count' + str(num_shots) + '_with_noise_knitted.pkl', 'wb') as file:
+         pickle.dump(circuit_knitter(circuit, 0, 10, num_shots, noise=True), file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 # In[ ]:
